435d61f75e3b4d72ff8d58bd95173835c3594c06
vivab <vivogaga12@gmail.com>
2026-08-21 12:16:24 +0500

    Update config.py

--- config.py
+++ config.py
@@ -7,5 +7,6 @@ DB_NAME = os.getenv("DB_NAME", "bot.db")
 MIN_WITHDRAWAL = float(os.getenv("MIN_WITHDRAWAL", "1.0"))
 CRYPTO_PAY_TOKEN = os.getenv("CRYPTO_PAY_TOKEN", "")
 CRYPTO_ASSET = os.getenv("CRYPTO_ASSET", "USDT")
+CRYPTO_PAY_API_URL = os.getenv("CRYPTO_PAY_API_URL", "https://pay.crypt.bot/api")
 
 AUTO_RELEASE_TIME = int(os.getenv("AUTO_RELEASE_TIME", "300"))
